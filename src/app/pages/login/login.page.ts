99e31ea9d1a9db759809cf175a5c13e991175dfb
Yadav <dyadav.contractor@rti.org>
2019-11-05 12:25:07 +0530

    alert added

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,31 +4,37 @@ import { auth } from 'firebase/app';
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { UserDetailsService } from 'src/app/services/user-details/user-details.service';
+import { SharedUtilService } from 'src/app/services/shared-util/shared-util.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-userName: string;
-password: string;
-  constructor(public afAuth: AngularFireAuth, private navCtrl: NavController, private storage: Storage,
-    private userService: UserDetailsService) { }
+  userName: string;
+  password: string;
+  constructor(
+    public afAuth: AngularFireAuth,
+    private navCtrl: NavController,
+    private storage: Storage,
+    private userService: UserDetailsService,
+    private sharedUtilService: SharedUtilService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async login() {
     try {
-   const res = await this.afAuth.auth.signInWithEmailAndPassword(this.userName + '@gmail.com', this.password);
-   console.log('printing successfull login res ', res);
-   if (res && res.user.uid) {
-     this.storage.set('userId', res.user.uid);
-     this.userService.setUId(res.user.uid);
-     this.navCtrl.navigateForward(['/milk-home']);
-   }
+      const res = await this.afAuth.auth.signInWithEmailAndPassword(this.userName + '@gmail.com', this.password);
+      console.log('printing successfull login res ', res);
+      if (res && res.user.uid) {
+        this.storage.set('userId', res.user.uid);
+        this.userService.setUId(res.user.uid);
+        this.navCtrl.navigateForward(['/milk-home']);
+      }
     } catch (error) {
+      this.sharedUtilService.createAlert('Invalid usesname/ passward!!!');
       console.dir(error);
     }
   }
